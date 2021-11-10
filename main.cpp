f886dee9c1404ce907082a97b80b13e37587f6bb
LauraMariaChiara <91607635+LauraMariaChiara@users.noreply.github.com>
2021-11-10 17:11:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main() {
     int n=0, x1=0, x2=1, xf=x1+x2;
     cin >> n;
-    cout << x2 << endl;
-    for(int i=3;i<=n;i++){
+    cout << "1" << endl;
+    for(int i=0;i<=(n-1);i++){
         cout << xf << endl;
         x1 = x2;
         x2 = xf;
