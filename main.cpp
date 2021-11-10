6970c8e2235d09098c82ed7108f6185606c20655
LauraMariaChiara <91607635+LauraMariaChiara@users.noreply.github.com>
2021-11-10 17:03:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, x1=0, x2=1, xf=x1+x2;
+    cin >> n;
+    cout << x2 << endl;
+    for(int i=3;i<=n;i++){
+        cout << xf << endl;
+        x1 = x2;
+        x2 = xf;
+        xf = x1 + x2;
+    }
+    return 0;
 }
